9928214a9f5d8d7e1001cb43c5921865d6fa4cda
Billal Saidi <63353891+billal280@users.noreply.github.com>
2025-02-07 16:19:26 +0100

    added base

--- provider/resource_opensearch_action_groups.go
+++ provider/resource_opensearch_action_groups.go
@@ -1,1 +1,38 @@
-#TODO
+package provider 
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/structure"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/olivere/elastic/uritemplates"
+	elastic7 "github.com/olivere/elastic/v7"
+)
+
+var actionGroupsShema = map [string]*schema.Schema{
+		"body": {
+
+		}
+}
+
+func resourceOpensearchActionGroups() {}
+
+func resourceOpensearchActionGroupsCreate() {}
+
+func resourceOpensearchActionGroupsRead()  {}
+
+func resourceOpensearchActionGroupsUpdate() {}
+
+func resourceOpensearchActionGroupsGet() {}
+
+func resourceOpensearchPostActionGroups() {}
+
+func resourceOpensearchPutActionGroups() {}
+
+func resourceOpensearchActionGroupsDelete() {}
+
+type actionGroupsResponse struct {}
